c2540eb62768303f37688e2e799b94fc3e899d30
Christos Metaxas <metxri1@hotmail.com>
2025-04-04 21:53:55 +0300

    Added tests and storybook frameworks

---
+++ learnhub_frontend/.storybook/preview.js
@@ -0,0 +1,15 @@
+import "../src/index.css"; // Import global CSS (π.χ., Tailwind)
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
